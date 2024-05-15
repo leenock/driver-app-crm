4941bb03c60d253fd5820dadccf6f18a408023ea
leenock <leenock176@gmail.com>
2024-05-15 15:57:21 +0300

    folder Structuring

--- frontend-side/src/App.js
+++ frontend-side/src/App.js
@@ -1,15 +1,6 @@
 import './App.css';
-import React, { useEffect } from "react";
 
 function App() {
-  const fetchData = async () => {
-    const res = await fetch("http://localhost:4000/");
-    console.log(res);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   return (
     <div className="App">
