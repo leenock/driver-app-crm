b11e63623574fb378424c86548a396aac5001eb9
leenock <leenock176@gmail.com>
2024-05-17 00:57:13 +0300

    how it works commits

--- frontend-side/src/components/Landingpage/index.jsx
+++ frontend-side/src/components/Landingpage/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Hero from '../Hero/Hero'
 import Why from '../Why/Why'
+import Hiw from '../How-it-works/Hiw'
 
 const Landingpage = () => {
   return (
     <>
     <Hero />
     <Why />
+    <Hiw />
+
     </>
   )
 }
