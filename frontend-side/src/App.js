f9cf223a06e574f59769419b6d97b64d608dbb2d
leenock <leenock176@gmail.com>
2024-05-16 23:06:21 +0300

    landing page commits

--- frontend-side/src/App.js
+++ frontend-side/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-
+import React from "react";
+import Header from './components/Header/Header';
+import Landingpage from './components/Landingpage';
 function App() {
-
   return (
     <div className="App">
-      <h1>Test</h1>
+       <div>
+        <Header />
+        <Landingpage />
+       </div>
+     
     </div>
   );
 }
---
+++ frontend-side/src/components/Landingpage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../Hero/Hero'
+
+const Landingpage = () => {
+  return (
+    <>
+    <Hero />
+    </>
+  )
+}
+
+export default Landingpage
