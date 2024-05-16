a6795e4c5e8366a12f6bdebf9e162e8fc49b56bb
leenock <leenock176@gmail.com>
2024-05-16 23:15:31 +0300

    landing page commit

--- frontend-side/src/components/Header/Header.jsx
+++ frontend-side/src/components/Header/Header.jsx
@@ -1,9 +1,156 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import "./header.css";
+import "./mediaquery.css";
 
 const Header = () => {
+  const [navActive, setNavActive] = useState(false);
+  const toggleNav = () => {
+    setNavActive(!navActive);
+  };
+  const closeMenu = () => {
+    setNavActive(false);
+  };
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 500) {
+        closeMenu();
+      }
+    };
+    const header = document.querySelector("[data-header]");
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        header.classList.add("active");
+      } else {
+        header.classList.remove("active");
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div>Header section</div>
-  )
-}
-
-export default Header
+    <header className="header" data-header>
+      <div className="container" >
+        <a href="/#" className="logo">
+          <img
+            src="./assets/images/light.png"
+            width="74"
+            height="24"
+            alt="Adexhome"
+            className="logo-light"
+          />
+          <img
+            src="./assets/images/dark.png"
+            width="74"
+            height="24"
+            alt="Adexhome"
+            className="logo-dark"
+          />
+        </a>
+        <nav
+          className={`navbar ${navActive ? "active" : "navbar.active"}`}
+          data-navbar onClick={closeMenu}
+        >
+          <div className="navbar-top">
+            <a href="/#" className="logo">
+              <img
+                src="./assets/images/light.png"
+                width="74"
+                height="24"
+                alt="Adex home"
+              />
+            </a>
+            <button
+              onClick={closeMenu}
+              className="nav-close-btn"
+              aria-label="close menu"
+              data-nav-toggler
+            >
+              <ion-icon name="close-outline" aria-hidden="true"></ion-icon>
+            </button>
+          </div>
+          <ul className="navbar-list" onClick={closeMenu}>
+            <li>
+              <a href="/#" className="navbar-link">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/#" className="navbar-link">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="/#" className="navbar-link">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="/#" className="navbar-link">
+                Blog
+              </a>
+            </li>
+            <li>
+              <a href="/#" className="navbar-link">
+                Contact
+              </a>
+            </li>
+          </ul>
+          <div className="wrapper">
+            <a href="mailto:info@email.com" className="contact-link">
+              info@email.com
+            </a>
+            <a href="tel:001234567890" className="contact-link">
+              254 (077) 456 78 90
+            </a>
+          </div>
+          <ul className="social-list">
+            <li>
+              <a href="/#" className="social-link">
+                <ion-icon name="logo-twitter"></ion-icon>
+              </a>
+            </li>
+            <li>
+              <a href="/#" className="social-link">
+                <ion-icon name="logo-facebook"></ion-icon>
+              </a>
+            </li>
+            <li>
+              <a href="/#" className="social-link">
+                <ion-icon name="logo-dribbble"></ion-icon>
+              </a>
+            </li>
+            <li>
+              <a href="/#" className="social-link">
+                <ion-icon name="logo-instagram"></ion-icon>
+              </a>
+            </li>
+            <li>
+              <a href="/#" className="social-link">
+                <ion-icon name="logo-youtube"></ion-icon>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <a href="/#" className="btn btn-primary">
+          Free Trial
+        </a>
+        <button
+          className={`nav-open-btn ${navActive ? "active" : ""}`}
+          onClick={toggleNav}
+        >
+          <ion-icon name="menu-outline" aria-hidden="true"></ion-icon>
+        </button>
+        <div
+          className={`overlay ${navActive ? "active" : "overlay.active"}`}
+          data-nav-toggler
+          data-overlay
+        ></div>
+      </div>
+    </header>
+  );
+};
+export default Header;
--- frontend-side/src/components/Hero/Hero.jsx
+++ frontend-side/src/components/Hero/Hero.jsx
@@ -1,9 +1,115 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import "./hero.css";
+import "./mediaquery.css";
 
 const Hero = () => {
-  return (
-    <h1>Hero section</h1>
-  )
+    const sliderRef = useRef(null);
+    useEffect(() => {
+        const initSlider = (slider) => {
+          const sliderContainer = slider.querySelector("[data-slider-container]");
+          const sliderPrevBtn = slider.querySelector("[data-slider-prev]");
+          const sliderNextBtn = slider.querySelector("[data-slider-next]");
+    
+          let currentSlidePos = 0;
+    
+          const moveSliderItem = () => {
+            sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+          };
+    
+          const slideNext = () => {
+            const slideEnd = currentSlidePos >= sliderContainer.childElementCount - 1;
+    
+            if (slideEnd) {
+              currentSlidePos = 0;
+            } else {
+              currentSlidePos++;
+            }
+    
+            moveSliderItem();
+          };
+    
+          sliderNextBtn.addEventListener("click", slideNext);
+    
+          const slidePrev = () => {
+            if (currentSlidePos <= 0) {
+              currentSlidePos = sliderContainer.childElementCount - 1;
+            } else {
+              currentSlidePos--;
+            }
+    
+            moveSliderItem();
+          };
+    
+          sliderPrevBtn.addEventListener("click", slidePrev);
+    
+          const dontHaveExtraItem = sliderContainer.childElementCount <= 1;
+          if (dontHaveExtraItem) {
+            sliderNextBtn.style.display = "none";
+            sliderPrevBtn.style.display = "none";
+          }
+        };
+    
+        const sliders = document.querySelectorAll("[data-slider]");
+        sliders.forEach((slider) => initSlider(slider));
+    
+        return () => {
+          // Cleanup code if needed
+        };
+      }, []);
+    
+
+    return (
+        <section className="section hero has-bg-image" aria-label="home" 
+          style={{ backgroundImage: "url('./assets/images/hero-bg.jpg')" }}>
+            <div className="container">
+                <div className="hero-content">
+                    <h1 className="h1 hero-title">Crafting project specific solutions with expertise.</h1>
+                    <p className="hero-text">
+                        We’re a creative company that focuses on establishing long-term relationships with customers.
+                    </p>
+                    <div className="btn-wrapper">
+                        <a href="/#" className="btn btn-primary">Explore Now</a>
+                        <a href="/#" className="btn btn-outline">Contact Us</a>
+                    </div>
+                </div>
+                <div className="hero-slider" data-slider ref={sliderRef}>
+                    <div className="slider-inner">
+                        <ul className="slider-container" data-slider-container>
+                            <li className="slider-item">
+                            <figure className="img-holder" style={{ "--width": 575, "--height": 550 }}>
+                                    <img src="./assets/images/hero-slide-1.jpg" style={{ "--width": 575, "--height": 550 }} alt="" className="img-cover" />
+                                </figure>
+                            </li>
+                            <li className="slider-item">
+                                <div className="hero-card">
+                                    <figure className="img-holder" style={{ "--width": 575, "--height": 550 }}>
+                                        <img src="./assets/images/hero-slide-2.jpg" width="575" height="550" alt="hero banner"
+                                            className="img-cover" />
+                                    </figure>
+
+                                    <button className="play-btn" aria-label="play adex intro">
+                                        <ion-icon name="play" aria-hidden="true"></ion-icon>
+                                    </button>
+                                </div>
+                            </li>
+                            <li className="slider-item">
+                                <figure className="img-holder" style={{ "--width": 575, "--height": 550 }}>
+                                    <img src="./assets/images/hero-slide-3.jpg" width="575" height="550" alt="" className="img-cover" />
+                                </figure>
+                            </li>
+                        </ul>
+                    </div>
+                    <button className="slider-btn prev" aria-label="slide to previous" data-slider-prev>
+                        <ion-icon name="arrow-back"></ion-icon>
+                    </button>
+                    <button className="slider-btn next" aria-label="slide to next" data-slider-next>
+                        <ion-icon name="arrow-forward"></ion-icon>
+                    </button>
+                </div>
+            </div>
+        </section>
+
+    )
 }
 
 export default Hero
