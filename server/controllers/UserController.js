3b5c00255488951d1174976eb261e9e02e571d67
leenock <leenock176@gmail.com>
2024-06-03 12:54:29 +0300

    hash paswword

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,24 +1,27 @@
+const bcrypt = require('bcryptjs');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-
 const express = require('express');
 const app = express();
-
-
 app.use(express.json());
 
-const registeruser = async (req, res) => {
+const registerdriver = async (req, res) => {
     const { email, phoneNumber, city, password } = req.body;
     try {
-      const registeruser = await prisma.registeruser.create({
+      // Generate salt
+      const salt = bcrypt.genSaltSync(10);
+      // Hash the password
+      const hashedPassword = bcrypt.hashSync(password, salt);
+      // Create user with hashed password
+      const newUser = await prisma.registeruser.create({
         data: {
           email: email,
           phoneNumber: phoneNumber,
           city: city,
-          password: password,
+          password: hashedPassword,
         },
       });
-      res.status(201).json(registeruser);
+      res.status(201).json(newUser);
     } catch (error) {
       res.status(400).json({ msg: error.message });
     }
@@ -46,49 +49,9 @@ const getuser = async (req, res) => {
     }
   }
 
-  
-/////////User Sign up/////////
-
- const createProduct = async (req, res) => {
-  const { name, price } = req.body
-  try {
-      const product = await prisma.product.create({
-          data: {
-              name: name,
-              price: price,
-          },
-      })
-      res.status(201).json(product)
-  } catch (error) {
-      res.status(400).json({ msg: error.message })
-  }
-}
- const getProducts = async (req, res) => {
-  try {
-      const response = await prisma.product.findMany()
-      res.status(200).json(response)
-  } catch (error) {
-      res.status(500).json({ msg: error.message })
-  }
-}
-
- const getProductById = async (req, res) => {
-  try {
-      const response = await prisma.product.findUnique({
-          where: {
-              id: Number(req.params.id),
-          },
-      })
-      res.status(200).json(response)
-  } catch (error) {
-      res.status(404).json({ msg: error.message })
-  }
-}
 module.exports = {
-  registeruser,
+  registerdriver,
   getuser,
   getuserById,
-  getProducts,
-  getProductById,
-  createProduct,
+ 
 };
--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const {registeruser,getuser,getuserById, getProducts, getProductById, createProduct } = require('../controllers/UserController');
+const {registerdriver,getuser,getuserById } = require('../controllers/UserController');
 
 const router = express.Router();
 
-
 router.get('/drivers', getuser);
 router.get('/drivers/:id', getuserById);
-router.post('/drivers', registeruser);
+router.post('/drivers', registerdriver);
 
-router.get('/products', getProducts);
-router.get('/products/:id', getProductById);
-router.post('/products', createProduct);
 
 module.exports = router;
