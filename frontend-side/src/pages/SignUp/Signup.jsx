f48d75cd3f2d03a6a71402ed65eead29a6377dc6
leenock <leenock176@gmail.com>
2024-06-05 01:05:25 +0300

    alerts box updates

--- frontend-side/src/pages/SignUp/Signup.jsx
+++ frontend-side/src/pages/SignUp/Signup.jsx
@@ -1,11 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import "./signup.css";
-import Header from '../../components/Header/Header';
+import Header from "../../components/Header/Header";
+import {
+  validateEmail,
+  validatePhoneNumber,
+} from "../../utils/validations/validation";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [city, setCity] = useState("Nairobi");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setSuccess("");
+
+    // driver-reg validation
+    if (!validateEmail(email)) {
+      setError("Invalid email format");
+      return;
+    }
+    if (!validatePhoneNumber(phoneNumber)) {
+      setError("Phone number must be numeric and 10 characters long");
+      return;
+    }
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    try {
+      const response = await fetch("http://localhost:4000/drivers", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          phoneNumber,
+          city,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setSuccess(
+          "Registration successful! Use your Email and password to Login"
+        );
+        setEmail("");
+        setPhoneNumber("");
+        setCity("Nairobi");
+        setPassword("");
+        setConfirmPassword("");
+      } else {
+        setError(data.msg);
+      }
+    } catch (error) {
+      setError("An error occurred. Please try again later.");
+    }
+  };
+
   return (
     <section className="application">
-       <Header />
+      <Header />
       <div className="registration-container">
         <div className="left-column">
           <img
@@ -16,7 +80,19 @@ const Signup = () => {
         </div>
         <div className="right-column">
           <h1 className="form-title">Become a Driver</h1>
-          <form class="input-wrapper">
+          <form className="input-wrapper" onSubmit={handleSubmit}>
+            {error && (
+              <div className="alert-box error">
+                <span className="icon error-icon">⚠️</span>
+                <p>{error}</p>
+              </div>
+            )}
+            {success && (
+              <div className="alert-box success">
+                <span className="icon success-icon">✔️</span>
+                <p>{success}</p>
+              </div>
+            )}
             <div className="form-row">
               <label htmlFor="email">Email</label>
               <div className="email-input">
@@ -25,32 +101,41 @@ const Signup = () => {
                   name="email_address"
                   placeholder="Email Address"
                   required
-                  class="input-field-sn"
+                  autoComplete="email"
+                  className="input-field-sn"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
             </div>
             <div className="form-row">
               <label htmlFor="phone">PhoneNumber</label>
               <div className="phone-input">
-               
                 <input
                   type="tel"
                   id="phone"
                   placeholder="Mobile number"
-                  class="input-field-sn"
                   required
+                  autoComplete="tel-national"
+                  className="input-field-sn"
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
                 />
               </div>
             </div>
             <div className="form-row">
               <label htmlFor="city">City</label>
               <div className="custom-dropdown">
-                <select id="city" defaultValue="Nairobi" class="input-field-sn">
+                <select
+                  id="city"
+                  className="input-field-sn"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                >
                   <option value="Nairobi">Nairobi</option>
                   <option value="Mombasa">Mombasa</option>
                   <option value="Embu">Embu</option>
                   <option value="Kisumu">Kisumu</option>
-                  {/* Add more cities as needed */}
                 </select>
                 <div className="arrow-down"></div>
               </div>
@@ -63,31 +148,37 @@ const Signup = () => {
                   name="Pass_word"
                   placeholder="Password"
                   required
-                  class="input-field-sn"
+                  className="input-field-sn"
+                  autoComplete="new-password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
             </div>
             <div className="form-row">
-            <label htmlFor="Password">Confirm Password</label>
-            <div className="Password-input">
-            <input
+              <label htmlFor="Password">Confirm Password</label>
+              <div className="Password-input">
+                <input
                   type="password"
                   name="confirm_Pass_word"
                   placeholder="Confirm Password"
                   required
-                  class="input-field-sn"
+                  autoComplete="new-password"
+                  className="input-field-sn"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
                 />
-            </div>
+              </div>
             </div>
             <button type="submit2">Sign up as a Driver</button>
           </form>
           <br />
-         <p>By signing up, you agree to our Terms of Service and Privacy policy,
-         Already have an account? Log in ↗
-         </p>
+          <p>
+            By signing up, you agree to our Terms of Service and Privacy policy,
+            Already have an account? Log in ↗
+          </p>
         </div>
       </div>
-      
     </section>
   );
 };
