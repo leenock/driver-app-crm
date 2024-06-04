2516fa16fe4041d9a496d23e4e8acec741af63ae
leenock <leenock176@gmail.com>
2024-06-04 14:29:50 +0300

    validations commits

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -9,12 +9,38 @@ const registerdriver = async (req, res) => {
   const { email, phoneNumber, city, password } = req.body;
   // Regular expression for email validation
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ msg: "Invalid email format" });
+  }
+  // Validate phone number format (numbers only and 13 characters long)
+  const phoneRegex = /^\d{13}$/;
+  if (!phoneRegex.test(phoneNumber)) {
+    return res
+      .status(400)
+      .json({
+        msg: "Phone number must contain only numbers and be 13 characters long",
+      });
+  }
   try {
+    // Check if email already exists
+    const existingEmail = await prisma.registeruser.findUnique({
+      where: { email: email },
+    });
+    if (existingEmail) {
+      return res.status(400).json({ msg: "Email already exists" });
+    }
+    // Check if phone number already exists
+    const existingPhoneNumber = await prisma.registeruser.findUnique({
+      where: { phoneNumber: phoneNumber },
+    });
+    if (existingPhoneNumber) {
+      return res.status(400).json({ msg: "Phone number already exists" });
+    }
     // Generate salt
     const salt = bcrypt.genSaltSync(10);
     // Hash the password
     const hashedPassword = bcrypt.hashSync(password, salt);
+
     // Create user with hashed password
     const newUser = await prisma.registeruser.create({
       data: {
@@ -24,6 +50,7 @@ const registerdriver = async (req, res) => {
         password: hashedPassword,
       },
     });
+
     res.status(201).json(newUser);
   } catch (error) {
     res.status(400).json({ msg: error.message });
