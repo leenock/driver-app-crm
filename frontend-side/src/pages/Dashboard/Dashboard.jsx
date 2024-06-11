3fae446cdfc1dcc69a04c2ee3c736cc3efe5f56f
leenock <leenock176@gmail.com>
2024-06-11 23:31:37 +0300

    ...

--- frontend-side/src/pages/Dashboard/Dashboard.jsx
+++ frontend-side/src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useState } from "react";
+import Navbar from "../components/TopMenu/Navbar";
+import Sidebar from "../components/Sidebar-menu/Sidebar";
+import Main from "../components/Main/Main";
 
 const Dashboard = () => {
-  return (
-    <h1>Dashboard</h1>
-  )
-}
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-export default Dashboard
+  const openSidebar = () => {
+    setSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
+  return (
+    <div className="container-dashboard">
+      <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar} />
+      < Main />
+      <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar}  />
+     
+    </div>
+  );
+};
+
+export default Dashboard;
